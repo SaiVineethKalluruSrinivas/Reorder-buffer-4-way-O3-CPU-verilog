27d41de7cd5ae2f4b5ea747c07dfa9f0ba7f922e
SaiVineethKalluruSrinivas <skalluru@andrew.cmu.edu>
2019-10-13 01:33:01 -0700

    Added comments except for 4indexarray

--- iq.sv
+++ iq.sv
@@ -61,14 +61,22 @@ logic [31:0] busybitAcc;
 assign full = full_reg;
 assign full_reg = (full_count > 27);
 
-// Insertion
+//assigning temporary wires for incoming instructions (not inserting them yet)
 assign inserted_0_reg = {robIdx0, srcReg0_1, srcReg0_2, 1'b0, 1'b0, 1'b1}; 
 assign inserted_1_reg = {robIdx1, srcReg1_1, srcReg1_2, 1'b0, 1'b0, 1'b1}; 
 assign inserted_2_reg = {robIdx2, srcReg2_1, srcReg2_2, 1'b0, 1'b0, 1'b1};      
-assign inserted_3_reg = {robIdx3, srcReg3_1, srcReg3_2, 1'b0, 1'b0, 1'b1}; 
+assign inserted_3_reg = {robIdx3, srcReg3_1, srcReg3_2, 1'b0, 1'b0, 1'b1};
+//calculating number of instructions to be inserted in this cycle
 assign insert_amount = (full_reg) ? 0:({2'b0, inserted[0]} + {2'b0, inserted[1]} + {2'b0, inserted[2]} + {2'b0, inserted[3]});
 
-// Reg Monitoring & Finding ready mask
+
+//Combination module below sweeps through the entire IQ and performs the following
+//1) Performs tag match of each operand1 and sets bit in executedReg1 (not changing the iq yet)
+//2) Performs tag match of each operand2 and sets bit in executedReg2 (not changing the iq yet)
+//3) If no execution has happened in this cycle, set bits to value from previous cycle's iq
+//4) Look if both operands are set (from the iq of previous cycle) for all IQ slots and create a mask of ready bits
+//5) Look if top bit is '0' (from the iq of previous cycle) for all IQ slots and create a mask of free slots
+// both free_mask and ready_mask are calculated for each cycle
 always_comb begin
 for(int i = 0; i <32; i++) begin
         if(executed && iq[i][0]) begin
@@ -104,7 +112,9 @@ always_comb begin
 
 end
 
-
+//Combination module below assigns the busy bit of all IQ entry for next cycle.
+//Accumulates busybit for IQ entry as 1'b0 if the IQ entry is chosen to be issued else sets it to previous cycle's IQ[0] value
+//The busybits accumulated are not assigned to next cycle IQ yet.
 always_comb begin
 for (int k = 0; k < 32; k++) begin
     busybitAcc[k] = (k == four_index_array[0] || k== four_index_array[1] || k==four_index_array[2] || k==four_index_array[3]) ? 1'b0:iq[k][0];
@@ -128,14 +138,18 @@ always_comb begin
     four_index_array_insert[3] = (free_mask[3]) ? (6'd31 - ({5'b0,leastOneIndexMaskInsert[3][0]} + {5'b0,leastOneIndexMaskInsert[3][1]} + {5'b0,leastOneIndexMaskInsert[3][2]} + {5'b0,leastOneIndexMaskInsert[3][3]} + {5'b0,leastOneIndexMaskInsert[3][4]} + {5'b0,leastOneIndexMaskInsert[3][5]} + {5'b0,leastOneIndexMaskInsert[3][6]} + {5'b0,leastOneIndexMaskInsert[3][7]} + {5'b0,leastOneIndexMaskInsert[3][8]} + {5'b0,leastOneIndexMaskInsert[3][9]} + {5'b0,leastOneIndexMaskInsert[3][10]} + {5'b0,leastOneIndexMaskInsert[3][11]} + {5'b0,leastOneIndexMaskInsert[3][12]} + {5'b0,leastOneIndexMaskInsert[3][13]} + {5'b0,leastOneIndexMaskInsert[3][14]} + {5'b0,leastOneIndexMaskInsert[3][15]} + {5'b0,leastOneIndexMaskInsert[3][16]} + {5'b0,leastOneIndexMaskInsert[3][17]} + {5'b0,leastOneIndexMaskInsert[3][18]} + {5'b0,leastOneIndexMaskInsert[3][19]} + {5'b0,leastOneIndexMaskInsert[3][20]} + {5'b0,leastOneIndexMaskInsert[3][21]} + {5'b0,leastOneIndexMaskInsert[3][22]} + {5'b0,leastOneIndexMaskInsert[3][23]} + {5'b0,leastOneIndexMaskInsert[3][24]} + {5'b0,leastOneIndexMaskInsert[3][25]} + {5'b0,leastOneIndexMaskInsert[3][26]} + {5'b0,leastOneIndexMaskInsert[3][27]} + {5'b0,leastOneIndexMaskInsert[3][28]} + {5'b0,leastOneIndexMaskInsert[3][29]} + {5'b0,leastOneIndexMaskInsert[3][30]} + {5'b0,leastOneIndexMaskInsert[3][31]})):6'd63;
 end
 
+
+//Combination module below assigns IQ values to be updated for the next cycle
 always_comb begin
 for (int l = 0; l < 32; l++) begin
+    //if index matches first free index then insert first reg here
     if(l == four_index_array_insert[0] && inserted[0]) begin
         iq_n[l][0] = inserted_0_reg[0];
         iq_n[l][25:3] = inserted_0_reg[25:3];
         iq_n[l][2] = inserted_0_reg[2];
         iq_n[l][1] = inserted_0_reg[1];
     end
+    //if index matches second free index then insert second reg here
     else if(l == four_index_array_insert[1] && inserted[1]) begin
         iq_n[l][0] = inserted_1_reg[0];
         iq_n[l][25:3] = inserted_1_reg[25:3];
@@ -143,6 +157,7 @@ for (int l = 0; l < 32; l++) begin
         iq_n[l][1] = inserted_1_reg[1];
     end
 
+    //if index matches third free index then insert third reg here
     else if(l == four_index_array_insert[2] && inserted[2]) begin
         iq_n[l][0] = inserted_2_reg[0];
         iq_n[l][25:3] = inserted_2_reg[25:3];
@@ -150,6 +165,7 @@ for (int l = 0; l < 32; l++) begin
         iq_n[l][1] = inserted_2_reg[1];
     end
 
+    //if index matches fourth  free index then insert fourth reg here
     else if(l == four_index_array_insert[3] && inserted[3]) begin
         iq_n[l][0] = inserted_3_reg[0];
         iq_n[l][25:3] = inserted_3_reg[25:3];
@@ -157,6 +173,8 @@ for (int l = 0; l < 32; l++) begin
         iq_n[l][1] = inserted_3_reg[1];
     end
     else begin
+    //if the index is not an insert index, then use all the accumulation registers created uptill now and assign the value for IQ_next cycle to be
+    //value accumulated from IQ_previous cycle
        iq_n[l][0] = busybitAcc[l]; 
        iq_n[l][25:3] = iq[l][25:3];
        iq_n[l][2] = executedReg1[l];
@@ -171,18 +189,22 @@ assign empty_array_check[1] = (four_index_array[1] != 63);
 assign empty_array_check[2] = (four_index_array[2] != 63);
 assign empty_array_check[3] = (four_index_array[3] != 63);
 
+//If any of the indices chosen for issue is 63, then it is not valid and hence should not be counted towards num issued
+//the numIssued for next cycle is directly issued here
 assign is_valid_removed[0] = (empty_array_check[0]) ? 1'b1 : 1'b0;
 assign is_valid_removed[1] = (empty_array_check[1]) ? 1'b1 : 1'b0;
 assign is_valid_removed[2] = (empty_array_check[2]) ? 1'b1 : 1'b0;
 assign is_valid_removed[3] = (empty_array_check[3]) ? 1'b1 : 1'b0;
-
-// Output Assignment
 assign numIssued = ({2'b0, is_valid_removed[0]} + {2'b0, is_valid_removed[1]} + {2'b0, is_valid_removed[2]} + {2'b0, is_valid_removed[3]});
+
+// Assigining issued IQ entry's ROB index directly to outputs, making right ROB output values available on clock
 assign issued0 = (empty_array_check[0]) ? iq[four_index_array[0]][25:19] : 0; 
 assign issued1 = (empty_array_check[1]) ? iq[four_index_array[1]][25:19] : 0;
 assign issued2 = (empty_array_check[2]) ? iq[four_index_array[2]][25:19] : 0;
 assign issued3 = (empty_array_check[3]) ? iq[four_index_array[3]][25:19] : 0;
 
+// Set new IQ (or IQ for next cycle) on positive edge of the clock
+// Update full_count (keeps track of number of isnts in the IQ) also on clock
 always_ff @ (posedge clk) begin
 if (~reset) begin
     iq <= '{default:0};
